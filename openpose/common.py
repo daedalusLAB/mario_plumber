7a6e45ae89eedcf2f4344aaa84294c19778bc8f7
Raul Sanchez <raul@um.es>
2022-09-23 11:38:15 +0000

    catch exception if not person at all

--- openpose/common.py
+++ openpose/common.py
@@ -1,8 +1,7 @@
 import cv2
 import sys
 import argparse
-sys.path.append("/usr/local/python")
-from openpose import pyopenpose as op
+import pyopenpose as op
 
 bodyPoints = { 'Nose': 0, 'Neck': 1, 'RShoulder': 2, 'RElbow': 3, 'RWrist': 4, 'LShoulder': 5, 
           'LElbow': 6, 'LWrist': 7, 'MidHip': 8, 'RHip': 9, 'RKnee': 10, 'RAnkle': 11, 
@@ -18,19 +17,23 @@ def person_with_hands_in_image(image, openposeWrapper):
   datum.cvInputData = image
   openposeWrapper.emplaceAndPop(op.VectorDatum([datum]))
 
-  if datum.poseKeypoints.size == 0:
+  try:
+    if datum.poseKeypoints.size == 0:
+      return False
+    else:
+      if datum.poseKeypoints.shape[0] > 0:
+        for i in range(datum.poseKeypoints.shape[0]):
+          # print(datum.poseKeypoints[i][bodyPoints['Nose']] )
+          # print(datum.poseKeypoints[i][bodyPoints['RWrist']] )
+          # print(datum.poseKeypoints[i][bodyPoints['LWrist']] )
+          return ( (datum.poseKeypoints[i][bodyPoints['Nose']] != [0,0,0]).all() and \
+            (datum.poseKeypoints[i][bodyPoints['RWrist']] != [0,0,0]).all() and \
+            (datum.poseKeypoints[i][bodyPoints['LWrist']] != [0,0,0]).all()
+          )
     return False
-  else:
-    if datum.poseKeypoints.shape[0] > 0:
-      for i in range(datum.poseKeypoints.shape[0]):
-        print(datum.poseKeypoints[i][bodyPoints['Nose']] )
-        print(datum.poseKeypoints[i][bodyPoints['RWrist']] )
-        print(datum.poseKeypoints[i][bodyPoints['LWrist']] )
-        return ( (datum.poseKeypoints[i][bodyPoints['Nose']] != [0,0,0]).all() and \
-          (datum.poseKeypoints[i][bodyPoints['RWrist']] != [0,0,0]).all() and \
-          (datum.poseKeypoints[i][bodyPoints['LWrist']] != [0,0,0]).all()
-        )
-  return False
+  except Exception as e:
+    return False
+
       
 
 # extract 15 frames from a given video separates in time intervals of video lenght / 16 
@@ -72,7 +75,11 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    params = dict()
+    params["model_folder"] = "/openpose/models/"
     openposeWrapper = op.WrapperPython()
+    openposeWrapper.configure(params)
+    openposeWrapper.start()
 
     frames = []
     frames = extract_frames(args.video)
